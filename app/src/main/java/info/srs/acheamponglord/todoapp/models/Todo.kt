001ba947dbc687b2c696f027abbed7afb2413d1d
LORD KAY <lord.acheampong@callenssolutions.com>
2020-05-27 12:09:43 +0000

    Add database annotations to the model class

--- app/src/main/java/info/srs/acheamponglord/todoapp/models/Todo.kt
+++ app/src/main/java/info/srs/acheamponglord/todoapp/models/Todo.kt
@@ -1,3 +1,10 @@
 package info.srs.acheamponglord.todoapp.models
 
-data class Todo (var name: String, var description: String, var isCompleted: Boolean)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Todo (@PrimaryKey val uid: Int,
+                 var name: String,
+                 var description: String?,
+                 var isCompleted: Boolean)
