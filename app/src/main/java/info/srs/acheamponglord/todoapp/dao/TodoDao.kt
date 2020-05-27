da358d133e8b4dba3417969974d04f33ffc6e16f
LORD KAY <lord.acheampong@callenssolutions.com>
2020-05-27 12:09:26 +0000

    Define the select and details dao for the todo

--- app/src/main/java/info/srs/acheamponglord/todoapp/dao/TodoDao.kt
+++ app/src/main/java/info/srs/acheamponglord/todoapp/dao/TodoDao.kt
@@ -1,8 +1,15 @@
 package info.srs.acheamponglord.todoapp.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
+import info.srs.acheamponglord.todoapp.models.Todo
 
 @Dao
-interface Todo {
+interface TodoDao {
+    @Query("SELECT * FROM todo")
+    fun getAll(): LiveData<List<Todo>>
 
+    @Query("SELECT * FROM  todo WHERE uid = :uid")
+    fun findByUID(uid: Int)
 }
