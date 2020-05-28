3d323072672b01167f7d812bd12a3c5891cbe0c5
LORD KAY <lord.acheampong@callenssolutions.com>
2020-05-28 13:58:43 +0000

    :recycle: Refactor the todo dao interface

--- app/src/main/java/info/srs/acheamponglord/todoapp/dao/TodoDao.kt
+++ app/src/main/java/info/srs/acheamponglord/todoapp/dao/TodoDao.kt
@@ -1,7 +1,10 @@
 package info.srs.acheamponglord.todoapp.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import info.srs.acheamponglord.todoapp.models.Todo
 
 @Dao
@@ -9,11 +12,11 @@ interface TodoDao {
     @Query("SELECT * FROM todo")
     fun getAll(): LiveData<List<Todo>>
 
-    @Query("SELECT * FROM  todo WHERE uid = :uid")
-    fun findByUID(uid: Int)
-
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(todo: Todo)
+
+    @Query("UPDATE todo SET name = :name, description = :description WHERE uid = :uid")
+    suspend fun update(uid: Int, name: String, description: String?)
 
     @Query("DELETE FROM todo")
     suspend fun deleteAll()
