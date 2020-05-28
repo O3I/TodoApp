9b350da6dcf16ad83df3aa99e6384044eed70961
LORD KAY <lord.acheampong@callenssolutions.com>
2020-05-28 14:28:18 +0000

    Add click handler to the fab to navigate to the new todo list activity

--- app/src/main/java/info/srs/acheamponglord/todoapp/TodoListActivity.kt
+++ app/src/main/java/info/srs/acheamponglord/todoapp/TodoListActivity.kt
@@ -1,12 +1,19 @@
 package info.srs.acheamponglord.todoapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import info.srs.acheamponglord.todoapp.databinding.ActivityMainBinding
 
 class TodoListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.addTodo.setOnClickListener {
+            val intent = Intent(this, AddTodoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
