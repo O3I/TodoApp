eeff9d344d2905a42447ba153a2c503e069c7669
LORD KAY <lord.acheampong@callenssolutions.com>
2020-06-01 14:07:16 +0000

    Override the onActivityResult to check the intent extras from the addtodo activity

--- app/src/main/java/info/srs/acheamponglord/todoapp/TodoListActivity.kt
+++ app/src/main/java/info/srs/acheamponglord/todoapp/TodoListActivity.kt
@@ -1,19 +1,43 @@
 package info.srs.acheamponglord.todoapp
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import info.srs.acheamponglord.todoapp.databinding.ActivityMainBinding
+import info.srs.acheamponglord.todoapp.viewmodels.TodoViewModel
 
 class TodoListActivity : AppCompatActivity() {
+    private lateinit var todoViewModel: TodoViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        todoViewModel = ViewModelProvider(this).get(TodoViewModel::class.java)
+
+
+
         binding.addTodo.setOnClickListener {
-            val intent = Intent(this, AddTodoActivity::class.java)
-            startActivity(intent)
+            val intent = Intent(this@TodoListActivity, AddTodoActivity::class.java)
+            startActivityForResult(intent, todoRequestCode)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == todoRequestCode && resultCode == Activity.RESULT_OK) {
+
+        } else {
+
+        }
+    }
+
+    companion object {
+        private const val todoRequestCode = 1
     }
 }
