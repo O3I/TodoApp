83c51d1c01aadfaa6bebc7c73b7b1659ec783be3
LORD KAY <lord.acheampong@callenssolutions.com>
2020-06-01 14:49:56 +0000

    :recycle: Rename the unit test file

--- app/src/test/java/info/srs/acheamponglord/todoapp/TodoUnitTest.kt
+++ app/src/test/java/info/srs/acheamponglord/todoapp/TodoUnitTest.kt
@@ -1,17 +1,27 @@
 package info.srs.acheamponglord.todoapp
 
+import info.srs.acheamponglord.todoapp.models.Todo
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class TodoUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun sameObject_Value() {
+        val todo = Todo(
+            1, "Go to the grocery store",
+            "Buying things like milk,sugar", false
+        )
+        assertEquals(todo.uid, 1)
+        assertEquals(todo.isCompleted, false)
     }
 }
